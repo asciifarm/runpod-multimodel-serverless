beaab5235873a22ff88f58e641042d0d3c404593
asciifarm <90805922+asciifarm@users.noreply.github.com>
2026-01-03 05:48:54 +0100

    Delete apps directory

--- apps/sd3_medium/download_weights.py
+++
@@ -1,6 +0,0 @@
-
-from huggingface_hub import snapshot_download
-
-MODEL_ID = "stabilityai/stable-diffusion-3-medium"
-
-snapshot_download(repo_id=MODEL_ID, resume_download=True)
--- apps/svd_xt_1_1/download_weights.py
+++
@@ -1,6 +0,0 @@
-
-from huggingface_hub import snapshot_download
-
-MODEL_ID = "stabilityai/stable-video-diffusion-img2vid-xt-1-1"
-
-snapshot_download(repo_id=MODEL_ID, resume_download=True)
